f4c615ae8131bfd1b78b2cdbdd7e12444b1b4689
Burt <2902978956@qq.com>
2021-05-06 19:55:58 +0800

    init项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <span>hello Vue3</span>
+  <Ice/>
+</template>
+
+<script>
+import Ice from './components/Ice.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Ice:Ice
+  }
+}
+</script>
---
+++ src/components/Ice.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    第一个Vue组件
+  </div>
+</template>
