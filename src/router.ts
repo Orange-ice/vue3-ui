7e42e787afcd771d0eb16d40c780d7a8a32ac5f7
Burt <2902978956@qq.com>
2021-05-07 20:50:54 +0800

    官网框架基本完成

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,10 @@
 import { createWebHashHistory, createRouter } from 'vue-router';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
-import SwitchDemo from './components/SwitchDemo.vue'
+import SwitchDemo from './components/SwitchDemo.vue';
+import Button from './components/Button.vue';
+import Dialog from './components/Dialog.vue';
+import Tabs from './components/Tabs.vue';
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -11,8 +14,12 @@ export const router = createRouter({
     {
       path: '/doc',
       component: Doc,
+      redirect: '/doc/switch',
       children: [
-        { path: 'switch', component: SwitchDemo }
+        { path: 'switch', component: SwitchDemo },
+        { path: 'button', component: Button },
+        { path: 'dialog', component: Dialog },
+        { path: 'tabs', component: Tabs }
       ]
     }
   ]
@@ -20,4 +27,4 @@ export const router = createRouter({
 
 router.afterEach(() => {
   console.log('路由切换了');
-})
+});
