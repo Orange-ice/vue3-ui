e53074921107b73ddf70b1535bac31e8b892641a
Burt <2902978956@qq.com>
2021-05-07 18:01:21 +0800

    初步搭建 HTML 结构

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <header>
-    <span>导航</span> | <router-link to="/">ice</router-link> | <router-link to="/ice">ice2</router-link>
-  </header>
-  <br/>
   <router-view/>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import { createWebHashHistory, createRouter } from 'vue-router'
-import Ice from './components/Ice.vue'
-import Ice2 from './components/Ice2.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 const history = createWebHashHistory()
 const router = createRouter({
   history,
   routes: [
-    { path: '/', component: Ice },
-    { path: '/ice', component: Ice2 }
+    { path: '/', component: Home },
+    { path: '/doc', component: Doc }
   ]
 })
 
