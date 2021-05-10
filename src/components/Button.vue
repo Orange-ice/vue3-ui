d7b5a951d704e228df43545af196a5a222cd20bd
Burt <2902978956@qq.com>
2021-05-10 13:12:13 +0800

    feat: Button theme属性

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,9 @@
 <template>
   <h3>Button 示例</h3>
   <Button>你好</Button>
+  <Button theme="button">你好</Button>
+  <Button theme="link">你好</Button>
+  <Button theme="text">你好</Button>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,9 +1,16 @@
 <template>
-  <button>
+  <button class="ice-button" :class="`theme-${theme}`">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
-export default {}
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
 </script>
