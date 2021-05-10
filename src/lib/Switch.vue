dbe2e210e52c631d9b7f1b144883f902205db30e
Burt <2902978956@qq.com>
2021-05-10 10:36:14 +0800

    Style：Switch组件基本样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -7,3 +7,25 @@
 <script lang="ts">
 export default {}
 </script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button{
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: blue;
+  border-radius: $h/2;
+  position: relative;
+}
+span{
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  width: $h2;
+  height: $h2;
+  border-radius: $h2/2;
+  background: #ffffff;
+}
+</style>
