1e274066555e79c6225bd436e94792c5f871f941
Burt <2902978956@qq.com>
2021-05-10 12:55:23 +0800

    feat: 创建Button组件

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    Button 示例
-  </div>
+  <h3>Button 示例</h3>
+  <Button>你好</Button>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+  components: { Button }
+}
+</script>
