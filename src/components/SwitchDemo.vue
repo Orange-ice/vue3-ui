ca9c73dd6222dacab194e6aff8f113125cbbfaf1
Burt <2902978956@qq.com>
2021-05-10 11:31:59 +0800

    feat: 使用 v-model

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <Switch />
+    <Switch v-model:value="checked" />
   </div>
 </template>
 
 <script lang="ts">
 import Switch from '../lib/Switch.vue'
+import { ref } from 'vue';
 export default {
-  components: { Switch }
+  components: { Switch },
+  setup() {
+    const checked = ref(false);
+    return { checked }
+  }
 }
 </script>
