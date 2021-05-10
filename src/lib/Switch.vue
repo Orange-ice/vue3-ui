946f572c87fa00bf02330b5e244997ce6f13802b
Burt <2902978956@qq.com>
2021-05-10 10:45:39 +0800

    feat: Switch 切换功能

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <button><span></span></button>
+    <button :class="{checked}" @click="toggle"><span></span></button>
   </div>
 </template>
 
 <script lang="ts">
-export default {}
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value
+    }
+    return { checked, toggle }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -15,9 +25,10 @@ button{
   height: $h;
   width: $h*2;
   border: none;
-  background: blue;
+  background: #cccccc;
   border-radius: $h/2;
   position: relative;
+  cursor: pointer;
 }
 span{
   position: absolute;
@@ -27,5 +38,11 @@ span{
   height: $h2;
   border-radius: $h2/2;
   background: #ffffff;
+}
+button.checked{
+  background: #2d8cf0;
+  > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
 }
 </style>
