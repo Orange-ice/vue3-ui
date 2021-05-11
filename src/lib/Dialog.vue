8d2fa2d9314102fb1f2021a7c3dc07d23fe01077
Burt <2902978956@qq.com>
2021-05-11 10:43:32 +0800

    feat: 创建 Dialog 组件

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ice-dialog-overlay"></div>
+  <div class="ice-dialog-wrapper">
+    <header>Title</header>
+    <main>
+      <p>content1</p>
+      <p>content2</p>
+    </main>
+    <footer>
+      <Button level="primary">确定</Button>
+      <Button>取消</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+  components: { Button }
+}
+</script>
