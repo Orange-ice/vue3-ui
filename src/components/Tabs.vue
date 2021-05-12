10bc83914e0592ec24706cd9dcd80c78c1a2768a
Burt <2902978956@qq.com>
2021-05-12 14:50:21 +0800

    feat: 创建 Tabs 和 Tab 组件，并渲染插槽内容。

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    Tabs 示例
+    <h3>Tabs 示例</h3>
+    <Tabs>
+      <Tab title="标题一">内容1</Tab>
+      <Tab title="标题二">内容二</Tab>
+    </Tabs>
   </div>
 </template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue';
+import Tab from '../lib/Tab.vue';
+export default {
+  components: {
+    Tabs,
+    Tab
+  },
+  setup() {}
+}
+</script>
