df89441fe96ca5758dedde589c21747f34907d13
Burt <2902978956@qq.com>
2021-05-12 20:41:22 +0800

    feat: 添加知识下划线，并实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="ice-tabs">
-    <div class="ice-tabs-nav">
+    <div class="ice-tabs-nav" ref="container">
       <div
           class="ice-tabs-nav-item"
           :class="{selected: t===selected}"
           v-for="(t, index) in titles"
           :key="index"
           @click="changeSelected(t)"
+          :ref="el => {if(el) navItems[index] = el }"
       >
         {{t}}
       </div>
+      <div class="ice-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="ice-tabs-content">
       <component class="ice-tabs-content-item" :is="current" :key="current.props.title" />
@@ -19,13 +21,16 @@
 
 <script lang="ts">
 import Tab from '../lib/Tab.vue';
-import { computed } from 'vue';
+import { computed, onMounted, onUpdated, ref } from 'vue';
 
 export default {
   props: {
     selected: String
   },
   setup (props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const container = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
     const defaults = context.slots.default();
     defaults.forEach(tag => {
       if (tag.type !== Tab) {
@@ -43,7 +48,18 @@ export default {
     const changeSelected = (title: string) => {
       context.emit('update:selected', title)
     }
-    return { defaults, titles, changeSelected, current };
+    const location = () => {
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0];
+      const { width, left: resultLeft } = result.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const { left: containerLeft } = container.value.getBoundingClientRect();
+      const left = resultLeft - containerLeft;
+      indicator.value.style.left = left + 'px'
+    }
+    onMounted(location);
+    onUpdated(location)
+    return { defaults, titles, changeSelected, current, container, indicator, navItems };
   }
 };
 </script>
@@ -58,6 +74,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item{
       padding: 8px 0;
@@ -65,6 +82,16 @@ $border-color: #d9d9d9;
       cursor: pointer;
       &:first-child{margin-left: 0;}
       &.selected{color: $blue;}
+    }
+
+    &-indicator{
+      position: absolute;
+      height: 3px;
+      width: 100px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      transition: all 250ms;
     }
   }
 
